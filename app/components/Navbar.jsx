9bd16534915019a8b6186d60368ebbdac88628ac
Zongyuan Yu <zongyuan1998@gmail.com>
2024-07-12 21:48:29 -0700

    update navmenu

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,51 +2,52 @@ import Link from "next/link";
 
 export default function NavMenu() {
   return (
-    <nav className="w-full bg-white bg-opacity-95 border-b-2 border-green-600 fixed top-0 left-0 z-50">
-      <div className="flex px-4 py-6 justify-start items-center">
-        {/* logo - home button */}
-        <div className="flex px-16">
-          <Link
-            href="/"
-            className="text-green-500 hover:text-blue-900 text-lg font-bold"
-          >
-            Fin Tools
-          </Link>
-        </div>
+    //flex-row with items-center: place items along the middle of crossaxis (Y axis for flex-row)
+    //and justify-center: place items along middle of the main axis (X axis for flex-row)
+    // z-50-always shown when stacked; 'sticky-fixed when scrol' on top left; take full width;
+    <nav className="sticky flex flex-row items-center justify-center top-0 left-0 z-50 w-full border-b-2 border-green-600 bg-white bg-opacity-85">
+      {/* home button; 32X6 padding */}
+      <div className="px-20 py-6">
+        <Link
+          href="/"
+          className="text-green-500 hover:text-blue-900 text-lg font-bold"
+        >
+          Fin Tools
+        </Link>
+      </div>
 
-        {/* nav menu items */}
-        <div className="flex px-10">
-          <Link
-            href="/"
-            className="text-black hover:text-black text-medium font-bold px-6"
-          >
-            Dashboard
-          </Link>
-          <Link
-            href="/"
-            className="text-black hover:text-black text-medium font-bold px-6"
-          >
-            Charts
-          </Link>
-          <Link
-            href="/"
-            className="text-black hover:text-black text-medium font-bold px-6"
-          >
-            Workbench
-          </Link>
-          <Link
-            href="/"
-            className="text-black hover:text-black text-medium font-bold px-6"
-          >
-            Crypto
-          </Link>
-          <Link
-            href="/"
-            className="text-black hover:text-black text-medium font-bold px-6"
-          >
-            Macro
-          </Link>
-        </div>
+      {/* nav menu items; 10X6 padding*/}
+      <div className="px-10 py-6">
+        <Link
+          href="/"
+          className="text-black hover:text-black text-medium font-bold px-6"
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/"
+          className="text-black hover:text-black text-medium font-bold px-6"
+        >
+          Charts
+        </Link>
+        <Link
+          href="/"
+          className="text-black hover:text-black text-medium font-bold px-6"
+        >
+          Workbench
+        </Link>
+        <Link
+          href="/"
+          className="text-black hover:text-black text-medium font-bold px-6"
+        >
+          Crypto
+        </Link>
+        <Link
+          href="/"
+          className="text-black hover:text-black text-medium font-bold px-6"
+        >
+          Macro
+        </Link>
       </div>
     </nav>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,46 @@
 export default function Home() {
   return (
     <>
-      <h3>hello world</h3>
+      {/* margin 20 from top to offset navmenu */}
+      <div className="mt-0 px-10">
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello king</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3>hello world</h3>
+        <h3 className="pb-10">hello world</h3>
+      </div>
     </>
   );
 }
