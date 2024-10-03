5b849e244e221ee0a1a05b5df97ded0a831900b5
EC2 Default User <ec2-user@ip-172-31-27-173.us-west-1.compute.internal>
2024-10-03 02:57:42 +0000

    move off ec2; add everything

---
+++ backend/daily_pipelines_coin.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "----------------------------------------------------------------"
+echo "--- Start at $(date) ---"
+cd /home/ec2-user/financial_database
+
+# Track the start time of the entire script
+start_time=$(date +%s)
+
+# Clean up checkpoints
+echo "--- Checkpoints Clean Up ---"
+FILE="/home/ec2-user/financial_database/backend/data/checkpoints/coin_calc_pipeline.json"
+[ -f "$FILE" ] && rm "$FILE" && echo "$FILE deleted" || echo "$FILE does not exist"
+
+ 
+echo "--- Start running pipeline_coin_price_overview_updater---"
+pipeline_start_time=$(date +%s)
+/home/ec2-user/financial_database/venv/bin/python3 /home/ec2-user/financial_database/backend/pipeline_coin_price_overview_updater.py
+pipeline_end_time=$(date +%s)
+echo "--- Finish running coin pipeline_coin_price_overview_updater---"
+echo "--- pipeline_coin_price_overview_updater Duration: $((pipeline_end_time - pipeline_start_time)) seconds ---"
+
+ 
+echo "--- Start running pipeline_coin_hourly_price_updater---"
+pipeline_start_time=$(date +%s)
+/home/ec2-user/financial_database/venv/bin/python3 /home/ec2-user/financial_database/backend/pipeline_coin_hourly_price_updater.py
+pipeline_end_time=$(date +%s)
+echo "--- Finish running pipeline_coin_hourly_price_updater---"
+echo "--- pipeline_coin_hourly_price_updater Duration: $((pipeline_end_time - pipeline_start_time)) seconds ---"
+
+ 
+echo "--- Start running pipeline_coin_signal_updater_coint---"
+pipeline_start_time=$(date +%s)
+/home/ec2-user/financial_database/venv/bin/python3 /home/ec2-user/financial_database/backend/pipeline_coin_signal_updater_coint.py
+pipeline_end_time=$(date +%s)
+echo "--- Finish running pipeline_coin_signal_updater_coint---"
+echo "--- pipeline_coin_signal_updater_coint Duration: $((pipeline_end_time - pipeline_start_time)) seconds ---"
+
+ 
+# Track the end time of the entire script
+end_time=$(date +%s)
+echo "--- Finished at $(date) ---"
+echo "--- Total Duration: $((end_time - start_time)) seconds ---"
+echo "----------------------------------------------------------------"
